f2e8ba5e6e10f35ccc7d4a50beb0f8f209dd1759
Syed T. Akbar <sakbar01@icloud.com>
2020-03-20 21:11:43 -0500

    small change

--- ConferenceTracker/Repositories/IPresentationRepository.cs
+++ ConferenceTracker/Repositories/IPresentationRepository.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 
 namespace ConferenceTracker.Repositories
-{
+{ 
     public interface IPresentationRepository
     {
         public void Create(Presentation presentation);
